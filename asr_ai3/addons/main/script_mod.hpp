9b9ee6719c83a3d86a1236c6f4658d2a9de370a7
Robalo <rs@bzbit.com>
2014-04-21 04:16:03 +0300

    build 39

--- asr_ai3/addons/main/script_mod.hpp
+++ asr_ai3/addons/main/script_mod.hpp
@@ -1,12 +1,12 @@
 #define PREFIX asr_ai3
 #define MAJOR 0
 #define MINOR 9
-#define PATCHLVL 1
-#define BUILD 35
+#define PATCHLVL 3
+#define BUILD 39
 
 #define VERSION MAJOR.MINOR.PATCHLVL.BUILD
 #define VERSION_AR MAJOR,MINOR,PATCHLVL,BUILD
 
 // MINIMAL required version for the Mod. Components can specify others..
-#define REQUIRED_VERSION 1.14
+#define REQUIRED_VERSION 1.16
 #define ASR_AI_USERCFG_VERSION 2
