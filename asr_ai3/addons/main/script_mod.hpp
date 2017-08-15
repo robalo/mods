24f3d23a7c6c9f8e924bf1ca25d34d0d8c1ab640
Robalo <rs@bzbit.com>
2017-08-15 12:53:15 +0300

    update settings

--- asr_ai3/addons/main/script_mod.hpp
+++ asr_ai3/addons/main/script_mod.hpp
@@ -1,8 +1,8 @@
 #define PREFIX asr_ai3
 #define MAJOR 1
 #define MINOR 1
-#define PATCHLVL 3
-#define BUILD 101
+#define PATCHLVL 4
+#define BUILD 102
 
 #define VERSION MAJOR.MINOR.PATCHLVL.BUILD
 #define VERSION_AR MAJOR,MINOR,PATCHLVL,BUILD
