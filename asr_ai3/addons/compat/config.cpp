451674b4abc151b8289c938cbedb9ce88fc2ef4d
Robalo <rs@bzbit.com>
2018-02-17 12:21:36 +0200

    bwc

---
+++ asr_ai3/addons/compat/config.cpp
@@ -0,0 +1,20 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+	class asr_ai3_cfgcamouflage {
+		units[] = {};
+		weapons[] = {};
+        ammo[] = {};
+		requiredAddons[] = {};
+		requiredVersion = REQUIRED_VERSION;
+        author = "Robalo";
+	};
+	class asr_ai3_cfgvehicles {
+		units[] = {};
+		weapons[] = {};
+        ammo[] = {};
+		requiredAddons[] = {};
+		requiredVersion = REQUIRED_VERSION;
+        author = "Robalo";
+	};
+};
---
+++ asr_ai3/addons/compat/script_component.hpp
@@ -0,0 +1,3 @@
+#define COMPONENT compat
+#include "\x\asr_ai3\addons\main\script_mod.hpp"
+#include "\x\asr_ai3\addons\main\script_macros.hpp"
