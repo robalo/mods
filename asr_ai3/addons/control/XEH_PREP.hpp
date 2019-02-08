23580fdffca769c9ca36cb73149fc2965df83b8b
Dedmen Miller <dedmen@dedmen.de>
2019-02-08 14:47:58 +0100

    More compile at gamestart

---
+++ asr_ai3/addons/control/XEH_PREP.hpp
@@ -0,0 +1,8 @@
+PREP(copyMyStance);
+PREP(fireOnMyShot);
+PREP(getGearAt);
+PREP(onTeamSwitch);
+PREP(inventoryClosed);
+PREP(gunLights);
+PREP(flipGunLights);
+PREP(unFreeze);
--- asr_ai3/addons/danger/config.cpp
+++ asr_ai3/addons/danger/config.cpp
@@ -18,6 +18,10 @@ class CfgPatches {
 
 PRELOAD_ADDONS;
 
+class Extended_PreStart_EventHandlers {
+	ADDON = QUOTE(call COMPILE_FILE(XEH_preStart));
+};
+
 class Extended_PreInit_EventHandlers {
 	ADDON = QUOTE(call COMPILE_FILE(XEH_preInit));
 };
